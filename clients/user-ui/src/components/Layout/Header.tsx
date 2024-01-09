df53f18460fc059bcc76bf622cf2d9c26467b7eb
Nitesh Agarwal <niteshkumar.nk649@gmail.com>
2024-01-09 11:12:43 +0530

    nav bar

--- clients/user-ui/src/components/Layout/Header.tsx
+++ clients/user-ui/src/components/Layout/Header.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import styles from "@/src/utils/style";
+import { Button } from "@nextui-org/button";
+import React from "react";
+import NavItems from "../NavItems";
+import ProfileDropDown from "../ProfileDropDown";
 
 const Header = () => {
   return (
-    <header>
-        
+    <header className=" w-full bg-[#0F1524] ">
+      <div className="w-[90%] m-auto h-[80px] flex items-center justify-between">
+        <h1 className={`${styles.logo}`}>AmNite</h1>
+        <NavItems />
+        <ProfileDropDown />
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
